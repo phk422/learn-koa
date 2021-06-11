632b2e8a64d9fbed9a1bcb49e91f0732ce9ba134
codervae <1769476788@qq.com>
2021-06-11 17:04:26 +0800

    引入端口配置文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const Koa = require("koa");
 const userRouter = require("./router/userRouter.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = new Koa();
 
-app.use(userRouter.routes()).listen(3000, () => console.log("服务器启动了~"));
+app.use(userRouter.routes()).listen(process.env.PORT, () => console.log("服务器启动了~"));
